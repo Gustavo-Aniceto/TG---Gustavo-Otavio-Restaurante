87f300e320609e1ec469aebb56f24e978af9cdf8
Gustavo Henrique Aniceto <108236975+Gustavo-Aniceto@users.noreply.github.com>
2024-06-19 15:52:46 -0300

    alt

--- cardapio-online-main/js/script.js
+++ cardapio-online-main/js/script.js
@@ -21,11 +21,13 @@ function openModal(edit = false, crud = 0) {
     sNome.value = itens[cru].nome
     sFuncao.value = itens[crud].funcao
     sPreco.value = itens[crud].preco
+    sImagem.value = itens[crud].imagem
     id = crud
   } else {
     sNome.value = ''
     sCategoria.value = ''
     sPreco.value = ''
+    sImagem.value = ''
   }
   
 }
@@ -48,6 +50,7 @@ function insertItem(item, crud) {
     <td>${item.nome}</td>
     <td>${item.categoria}</td>
     <td>R$ ${item.preco}</td>
+    <td>${item.imagem}</td>
     <td class="acao">
       <button onclick="editItem(${crud})"><i class='bx bx-edit' ></i></button>
     </td>
@@ -60,7 +63,7 @@ function insertItem(item, crud) {
 
 btnSalvar.onclick = e => {
   
-  if (sNome.value == '' || sCategoria.value == '' || sPreco.value == '') {
+  if (sNome.value == '' || sCategoria.value == '' || sPreco.value == '' || sImagem.value == '') {
     return
   }
 
@@ -70,8 +73,9 @@ btnSalvar.onclick = e => {
     itens[id].nome = sNome.value
     itens[id].categoria = sCategoria.value
     itens[id].preco = sPreco.value
+    itens[id].imagem = sImagem.value
   } else {
-    itens.push({'nome': sNome.value, 'categoria': sCategoria.value, 'salario': sPreco.value})
+    itens.push({'nome': sNome.value, 'categoria': sCategoria.value, 'salario': sPreco.value, 'imagem': sImagem.value})
   }
 
   setItensBD()
@@ -97,12 +101,13 @@ loadItens()
 
 
 // Exemplo de adicionar um novo item à tabela após inclusão
-function adicionarNaTabela(nome, categoria, preco) {
+function adicionarNaTabela(nome, categoria, preco,imagem) {
   let tbody = document.querySelector('.divTable table tbody');
   let newRow = `<tr>
                   <td>${nome}</td>
                   <td>${categoria}</td>
                   <td>${preco}</td>
+                  <td>${imagem}</td>
                   <td class="acao">Editar</td>
                   <td class="acao">Excluir</td>
                 </tr>`;
@@ -115,15 +120,17 @@ btnSalvar.addEventListener('click', function(event) {
   let nome = document.getElementById('m-nome').value;
   let categoria = document.getElementById('m-categoria').value;
   let preco = document.getElementById('m-salario').value;
+  let imagem = document.getElementById('m-imagem').value;
 
   // Chame sua função de criar novo produto aqui, e então adicione à tabela
-  createProduct(categoria, { "name": nome, "category": categoria, "price": preco });
+  createProduct(categoria, { "name": nome, "category": categoria, "price": preco, "imagem":imagem });
 
   // Limpe os campos do formulário modal
   document.getElementById('m-nome').value = '';
   document.getElementById('m-categoria').value = '';
   document.getElementById('m-salario').value = '';
+  document.getElementById('m-imagem').value = '';
 
   // Adicione à tabela
-  adicionarNaTabela(nome, categoria, preco);
+  adicionarNaTabela(nome, categoria, preco, imagem);
 });
