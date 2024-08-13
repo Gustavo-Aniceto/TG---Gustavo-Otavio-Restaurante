c796e988af02fdd707fc732293d0c120f0f3d863
Gustavo Henrique Aniceto <108236975+Gustavo-Aniceto@users.noreply.github.com>
2024-08-13 20:45:04 -0300

    rotas atualizadas e corrigidas

--- cardapio-online-main/backend/server.js
+++ cardapio-online-main/backend/server.js
@@ -6,6 +6,8 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const app = express();
 const port = 3000;
+const Stripe = require('stripe');
+const stripe = Stripe('Aniceto');
 
 // Middleware
 app.use(cors());
@@ -17,10 +19,45 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Rotas
 const produtosRoutes = require('./routes/produtos');
 const categoriasRoutes = require('./routes/categorias');
+const usersRoutes = require('./routes/users');
+
 
 // Usando as rotas
 app.use('/api/produtos', produtosRoutes);
 app.use('/api/categorias', categoriasRoutes);
+app.use('/api/users', usersRoutes);
+
+
+app.post('/api/pagar', async (req, res) => {
+    try {
+        const { amount, currency, source } = req.body;
+
+        const charge = await stripe.charges.create({
+            amount,
+            currency,
+            source,
+            description: 'Descrição do pagamento',
+        });
+
+        res.json(charge);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.post('/api/create-checkout-session', async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: req.body.items,
+        mode: 'payment',
+        success_url: 'http://localhost:3000/success',
+        cancel_url: 'http://localhost:3000/cancel',
+    });
+
+    res.json({ id: session.id });
+});
+
+
 
 // Iniciar o servidor
 app.listen(port, () => {
